ea9c4a699c3e3c55bda6b30dfc9ca84d25c0014b
lilitttaa <128687876+lilitttaa@users.noreply.github.com>
2024-07-20 15:32:00 +0800

    [chore] Setup basePath.

--- nextjs.config.js
+++ nextjs.config.js
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-  // 去掉 `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 const nextConfig = {
-  basePath,
-  assetPrefix,
+  basePath:'blog-github',
   output: "export",
 };
 
